4b6b40b3db696db6b08df921b182c60a5f7ac228
Antonio Murdaca <runcom@linux.com>
2015-07-18 15:31:10 +0200

    handle install w/o wireless driver dir (i.e. you don't compile wifi drivers at all like I do)

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #/bin/sh
-# handle case where wireless folder isn't in k source see README
+if [ "$(id -u)" != "0" ]; then
+	echo "You should run this script as root"
+	exit
+fi
+wdir="/lib/modules/$(uname -r)/kernel/drivers/net/wireless"
+if [ ! -d $wdir ]; then
+	echo "creating wireless dir in ksource"
+	sudo mkdir -p $wdir
+fi
 make && sudo make install && sudo depmod && sudo modprobe wl && make clean
